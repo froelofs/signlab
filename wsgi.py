79669254e47656e57763a5d3f3a668afbd572af7
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-08-14 15:51:45 +0200

    suggesties via alert

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
-import cgi, cgitb
-    def index():
-        data = cgi.FieldStorage()
-        mydata = data['param'].value
-        return "Hello"
+from flask import request
+
+def pyfoo() : 
+	try:
+		data = json.loads(request.data)
+		print(data)
+		return "Success"
+	except ValueError:
+		error('Unable to parse JSON data from request.')
+		return "Error"
+
+if __name__ == '__main__':
+  pyfoo()
