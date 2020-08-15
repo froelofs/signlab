1fbd5f7f086a7bb1dd38aeaaaed749fe91792c24
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-08-16 01:05:34 +0200

    calling external python script

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-from browser import document
+from browser import document, alert
 from browser.widgets.dialog import InfoDialog
+from main import main
+from functionsAux import searchDict
 
 def click(ev):
     InfoDialog("Hello", f"Hello, {document['mySiGML'].value} !")
 
+def translate(ev):
+	result = main({document['mySiGML'].value})
+	alert(result)
+
+def matches(ev):
+	results = searchDict({document['mySiGML'].value})
+	alert(results)
+
 # bind event 'click' on button to function echo
-document["echo"].bind("click", click)
+document["echo"].bind("click", translate)
