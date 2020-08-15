e1ed4e70f8397c3df12e6501acf3daf69dc5aaa6
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-08-16 00:58:57 +0200

    testing brython

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from browser import document
 from browser.widgets.dialog import InfoDialog
 
 def click(ev):
-    InfoDialog("Hello", f"Hello, {document['zone'].value} !")
+    InfoDialog("Hello", f"Hello, {document['mySiGML'].value} !")
 
 # bind event 'click' on button to function echo
 document["echo"].bind("click", click)
