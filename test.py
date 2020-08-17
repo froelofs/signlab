827568b72c817cd8b30852fa7b275460d4b51ac2
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-08-17 20:24:50 +0200

    adding suggestions as divs to page

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def matches(ev):
 
 def update_input(ev):
 	selected = [option.value for option in sel if option.selected]
-	document['mySiGML'].value = selected[0].value
+	document['mySiGML'].value = selected[0]
 
 # def update_select(ev):
 #     # selects / deselects options in the SELECT box
