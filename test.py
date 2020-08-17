75bff22db1c3f35dfe5f9051c825fb1f6b727730
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-08-17 12:49:53 +0200

    testing program call

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def matches(ev):
 	results = searchDict(input)
 	alert(len(results))
 	if len(results):
-		alert("These are the results:" + results)
+		alert("These are the results: " + results)
 	else:
 		alert("No results were found")
 	
