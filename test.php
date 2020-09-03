cb41bfd3550ec953f557aacfd72e1e393a9087d7
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-09-03 18:24:10 +0200

    php test

--- test.php
+++ test.php
@@ -8,8 +8,15 @@
 // $handle = proc_open('echo Hello world, $USER!', $descriptors, $pipes, null, ['USER' => 'guest']);
 // $world = stream_get_contents($pipes[1]);
 // var_dump($world);
+	function debug_to_console($data) {
+	    $output = $data;
+	    if (is_array($output))
+	        $output = implode(',', $output);
 
+	    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+	}
 	// echo json_encode("hello world");
+	debug_to_console("text");
 	echo json_encode($_POST["text"]);
 	// get the q parameter from URL
 	// $q = $_REQUEST["q"];
