cde5a644cd3a0b2312a2b0daee1f234ccc366560
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-09-03 17:58:36 +0200

    php test

--- test.php
+++ test.php
@@ -9,7 +9,8 @@
 // $world = stream_get_contents($pipes[1]);
 // var_dump($world);
 
-	echo json_encode("hello world");
+	// echo json_encode("hello world");
+	echo json_encode($_POST["text"]);
 	// get the q parameter from URL
 	// $q = $_REQUEST["q"];
 	// echo q
