e15d15236a5a02456ce6b2d077c5d9999b6097ea
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-09-03 17:15:28 +0200

    php test

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
-<!-- // $exe_command = 'C:\\Windows\\System32\\ping.exe -t google.com';
+<?php
+// $exe_command = 'C:\\Windows\\System32\\ping.exe -t google.com';
 
 // $process = proc_open($exe_command, $descriptorspec, $pipes);
 
@@ -6,7 +7,7 @@
 // $descriptors = [['pipe', 'r'], ['pipe', 'w'], ['pipe', 'w']];
 // $handle = proc_open('echo Hello world, $USER!', $descriptors, $pipes, null, ['USER' => 'guest']);
 // $world = stream_get_contents($pipes[1]);
-// var_dump($world); -->
-<?php
+// var_dump($world);
+
 	echo "hello world";
 ?>
