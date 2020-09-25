97a4bb700f2a4b19fea8bd83138ceb742ca194a7
Floris Roelofsen <floris.roelofsen@gmail.com>
2020-09-25 21:57:30 +0200

    update

--- static/grayscalescript.js
+++ static/grayscalescript.js
@@ -30,10 +30,10 @@
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
+    // // Closes responsive menu when a scroll trigger link is clicked
+    // $(".js-scroll-trigger").click(function () {
+    //     $(".navbar-collapse").collapse("hide");
+    // });
 
     // Activate scrollspy to add active class to navbar items on scroll
     $("body").scrollspy({
@@ -41,16 +41,16 @@
         offset: 100,
     });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
+    // // Collapse Navbar
+    // var navbarCollapse = function () {
+    //     if ($("#mainNav").offset().top > 100) {
+    //         $("#mainNav").addClass("navbar-shrink");
+    //     } else {
+    //         $("#mainNav").removeClass("navbar-shrink");
+    //     }
+    // };
+    // // Collapse now if page is not at top
+    // navbarCollapse();
+    // // Collapse the navbar when page is scrolled
+    // $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
