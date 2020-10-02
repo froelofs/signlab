e1238f37ead1144f07dbff9269a473bdefe5ce4e
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-10-02 17:09:22 +0200

    website met call naar python via phph test

--- pythonCall.php
+++ pythonCall.php
@@ -2,7 +2,7 @@
 
 if(isset($_POST['input'])){
 	$text = $_POST['input'];
-	$command = escapeshellcmd('/var/www/illc//projects/signlanguage/pythonTest/main.py '. $text ."2>&1");
+	$command = escapeshellcmd('main.py '. $text ."2>&1");
 	$output = shell_exec($command);
 	echo $output;
 }
