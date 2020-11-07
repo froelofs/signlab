ea983800742d00b35d866d84ef092d5f185a2a9b
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-11-07 17:43:27 +0100

    add fade in to tabcontent

--- js/signlabJS.js
+++ js/signlabJS.js
@@ -134,7 +134,7 @@ function callPython(text) {
 $('a[class="nav-link tabFade"]').on('show.bs.tab', function (e) {
 	targetHref = e.target.href.split("#")[1];
 	// relatedHref = e.relatedTarget.href.split("#")[1];
-	relatedHref = document.getElementsByClassName("nav-link tabFade active")[0];
+	relatedHref = document.getElementsByClassName("nav-link tabFade active")[0] + "";
 	// var relatedHref = $(event.relatedTarget).attr('href');
 	console.log(typeof relatedHref);
 	console.log(relatedHref.split("#")[1]);
