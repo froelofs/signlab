faec053d344e2468a1a52126908325bbd375f329
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-11-09 20:51:57 +0100

    zelfgemaakte tab functie met fade in

--- js/signlabJS.js
+++ js/signlabJS.js
@@ -106,7 +106,7 @@ function openTab(evt, tabName) {
   tabcontent = document.getElementsByClassName("fadeIn");
   for (i = 0; i < tabcontent.length; i++) {
     if (tabcontent[i].className.includes("undisplayed") == false){
-      console.log(tabcontent[i].className);
+     console.log(tabcontent[i].className);
      tabcontent[i].className += " undisplayed";
     }
   }
