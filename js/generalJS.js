2d185b394f09ef64211b78efccae77fe15fff212
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-11-12 00:55:29 +0100

    alert probeersel

--- js/generalJS.js
+++ js/generalJS.js
@@ -56,7 +56,7 @@ function alertMessage (type, text, parent){
   a.setAttribute("data-dismiss","alert");
   a.setAttribute("aria-label","close");
   a.setAttribute("class","close");
-  a.append(document.createTextNode("&times;"));
+  a.html("&times;")
   alert.appendChild(a);
  
   var textNode = document.createTextNode(text);
