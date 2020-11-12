35f2f4e958285eb16cb59ff5ebbf79f8e0546e52
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-11-12 14:29:25 +0100

    alert probeersel

--- suggestions.php
+++ suggestions.php
@@ -9,7 +9,7 @@
 	else{
 		$input = $_REQUEST['input'] + "";
 
-		$fileName = "https://www.florisroelofsen.com/signlab/suggestions.txt";
+		$fileName = "https://github.com/froelofs/signlab/blob/master/suggestions.txt";
 		if ( !file_exists($fileName) ) {
 	    $error = "File not found";
 	  	}
