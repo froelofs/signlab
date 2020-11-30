5aae8ef68eb24782cc709f8718ae9ebcbd9411a0
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-11-30 23:57:46 +0100

    digitale tijd vertaling werkt nu ook in het Engels

--- js/generalJS.js
+++ js/generalJS.js
@@ -84,35 +84,44 @@ $('.timepicker').timepicker({
 
   function adaptTime(time,language="NL"){
     time = time.split(":");
+    hour = parseInt(time[0]);
+    minutes = time[1];
+
+    if (hour > 12){
+     hour = hour - 12;
+    }
+
+    hour = hour.toString();
+      
+    // Rounds the minutes to the nearest option
+    if (minutes.charAt(1) == "1" || minutes.charAt(1) == "2"){
+     minutes = minutes.charAt(0) + "0";
+    }
+    else if (minutes.charAt(1) == "3" || minutes.charAt(1) == "4"){
+     minutes = minutes.charAt(0) + "5";
+    }
+
+    //Translates the time according to the language the page is set to
     if (language == "EN"){
-      time = time[0] + time[1];
+      convert = {"05":"5 past","10":"10 past","15":"quarter past","20":"20 past","25":"25 past","30":"half past","35":"25 to",
+      "40":"20 to","45":"quarter to","50":"10 to","55":"5 to"};
+
+      if (parseInt(minutes) > 30){
+       hour = parseInt(hour) + 1;
+      }
+      
+      minutes = convert[minutes];
     } 
     else if (language == "NL"){
       convert = {"05":"5 over","10":"10 over","15":"kwart over","20":"10 voor half","25":"5 voor half","30":"half","35":"5 over half",
       "40":"10 over half","45":"kwart voor","50":"10 voor","55":"5 voor"};
-      hour = parseInt(time[0]);
-      minutes = time[1];
-
-      if (hour > 12){
-        hour = hour - 12;
-      }
-
-      hour = hour.toString();
-      
-      // Rounds the minutes to the nearest option
-      if (minutes.charAt(1) == "1" || minutes.charAt(1) == "2"){
-       minutes = minutes.charAt(0) + "0";
-      }
-      else if (minutes.charAt(1) == "3" || minutes.charAt(1) == "4"){
-       minutes = minutes.charAt(0) + "5";
-      }
 
       if (parseInt(minutes) > 25){
        hour = parseInt(hour) + 1;
       }
-
+      
       minutes = convert[minutes];
-      time = minutes + " " + hour; 
     }
-  return time;
+   time = minutes + " " + hour; 
+   return time;
   }
