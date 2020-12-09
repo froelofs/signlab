680d9c9d68715b7179f42e606968a4872eb9c3eb
ShaniMG <shani.mende-gillings@student.uva.nl>
2020-12-09 14:06:02 +0100

    minutes test

--- covid19/zonmwJS.js
+++ covid19/zonmwJS.js
@@ -149,10 +149,7 @@ $( function() {
 function checkText(text,value=-1){
   // Makes all the variable boxes invisible
   elements = [...document.getElementsByClassName('varBox')];
-  console.log(typeof elements);
-  console.log(elements);
   elements.forEach(function(element){
-    console.log(element);
     element.style.display = 'none';
   });
 
