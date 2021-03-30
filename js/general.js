da760c5f27af53582aaf166393eee5cd1cd02c68
LykeEsselink <esselink97@gmail.com>
2021-03-30 23:11:38 +0200

    getpage

--- js/general.js
+++ js/general.js
@@ -178,9 +178,12 @@ $('.timepicker').timepicker({
   //navbar test
   $(function(){
     $("#nav-placeholder").load("nav.html");
+
+    getPage();
+    // $("#" + id).addClass("active");
   });
 
-  $(function changeActive() {
+function getPage() {
   var tablinks, newTab, allTabs, sections, i, j, current;
   // Get all elements with class="nav-link" and remove the class "active"
   tablinks = document.getElementsByClassName("nav-link");
@@ -199,10 +202,31 @@ $('.timepicker').timepicker({
       current.className = current.className.replace("nav-link", "nav-link active");
     }
   }
-  // var newTab = document.getElementById(tabName);
-  // console.log(newTab.className);
-  // newTab.className = newTab.className.replace("nav-link", "nav-link active");
-});
+}
+
+//   $(function changeActive() {
+//   var tablinks, newTab, allTabs, sections, i, j, current;
+//   // Get all elements with class="nav-link" and remove the class "active"
+//   tablinks = document.getElementsByClassName("nav-link");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//
+//   allTabs = ['AboutNav', 'TranslateNav', 'PeopleNav', 'PublicationsNav', 'ProjectsNav'];
+//   sections = ['About', 'Translate', 'People', 'Publications', 'Projects'];
+//   // Add an "active" class to the button that opened the tab
+//   for (j = 0; j < sections.length; j++) {
+//     if (document.getElementById(sections[j])) {
+//       current = document.getElementById(allTabs[j]);
+//       console.log(current);
+//       console.log(current.className);
+//       current.className = current.className.replace("nav-link", "nav-link active");
+//     }
+//   }
+//   // var newTab = document.getElementById(tabName);
+//   // console.log(newTab.className);
+//   // newTab.className = newTab.className.replace("nav-link", "nav-link active");
+// });
 
   function openTab(evt, tabName) {
     // Declare all variables
