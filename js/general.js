7e25f256fd1c6503b3f3596abf76edb84b9355e0
LykeEsselink <esselink97@gmail.com>
2021-03-31 00:19:12 +0200

    log translate

--- js/general.js
+++ js/general.js
@@ -205,6 +205,7 @@ $('.timepicker').timepicker({
 
 
     var id = getPage();
+    console.log(id);
     $("#" + id).addClass("active");
   });
 
@@ -219,29 +220,6 @@ function getPage(){
   }
 }
 
-// function changeActive(tabName) {
-//   var tablinks, newTab, i;
-//   // Get all elements with class="nav-link" and remove the class "active"
-//   tablinks = document.getElementsByClassName("nav-link");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-//
-//   // allTabs = ['AboutNav', 'TranslateNav', 'PeopleNav', 'PublicationsNav', 'ProjectsNav'];
-//   // sections = ['About', 'Translate', 'People', 'Publications', 'Projects'];
-//   // // Add an "active" class to the button that opened the tab
-//   // for (j = 0; j < sections.length; j++) {
-//   //   if (document.getElementById(sections[j])) {
-//   //     current = document.getElementById(allTabs[j]);
-//   //     console.log(current);
-//   //     console.log(current.className);
-//   //     current.className = current.className.replace("nav-link", "nav-link active");
-//   //   }
-//   // }
-//   newTab = document.getElementById(tabName);
-//   console.log(newTab.className);
-//   newTab.className = newTab.className.replace("nav-link", "nav-link active");
-// });
 
   function openTab(evt, tabName) {
     // Declare all variables
