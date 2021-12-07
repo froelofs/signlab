35b2d3af9b2e31d1a73fe6c0919df42aa5fb4f5c
Britt van Gemert <britt-van-gemert@live.nl>
2021-12-07 15:20:00 +0100

    getallen tekstueel weergeven, gloss omschrijvingen vervoegd, vertrekgebaar vernieuwd

--- ns-demo/js/tosigml.js
+++ ns-demo/js/tosigml.js
@@ -176,7 +176,7 @@ async function getSiGML(sentenceArray){
       console.log('Both undefined or element skipped: ', el);
     }
     if (el == lastItem){
-      tempString += '<hamgestural_sign gloss=""><sign_manual both_hands="true" lr_symm="true"><handconfig handshape="fist" thumbpos="across" /><handconfig extfidir="dl" /><handconfig palmor="l" /><location_bodyarm contact="touch" location="belowstomach" side="right_beside"><location_hand digits="1" /></location_bodyarm></sign_manual><sign_nonmanual><head_tier><head_movement movement="PB" /></head_tier><body_tier><body_movement movement="ST" /></body_tier><facialexpr_tier><eye_brows movement="RB" /><eye_lids movement="WB" /></facialexpr_tier></sign_nonmanual></hamgestural_sign>';
+      tempString += '<hamgestural_sign gloss=""><sign_manual both_hands="true" lr_symm="true"><handconfig handshape="fist" thumbpos="across" /><handconfig extfidir="dl" /><handconfig palmor="l" /><location_bodyarm contact="touch" location="belowstomach" side="right_beside"><location_hand digits="1" /></location_bodyarm></sign_manual><sign_nonmanual><body_tier><body_movement movement="ST" /></body_tier><facialexpr_tier><eye_brows movement="RB" /><eye_lids movement="WB" /></facialexpr_tier></sign_nonmanual></hamgestural_sign>';
       tempString += '</sigml>';
     }
   }
