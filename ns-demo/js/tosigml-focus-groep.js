2c58327236ea4bb0de1d6cdbc8dda828d350efb6
Britt van Gemert <britt-van-gemert@live.nl>
2022-02-11 11:54:31 +0100

    focus groep onderdelen: snelheid, pauze basis, stoppen tussengelegen stations

--- ns-demo/js/tosigml-focus-groep.js
+++ ns-demo/js/tosigml-focus-groep.js
@@ -173,6 +173,22 @@ function getSigmlVariables(entry, variableArray, stationsArray, av){
     variableArray.push("IB2");
   }
 
+  if(entry.includes("niet_Arnhem")){
+    variableArray.push("niet_Arnhem");
+  }
+
+  if(entry.includes("niet_Breda")){
+    variableArray.push("niet_Breda");
+  }
+
+  if(entry.includes("niet_Deventer")){
+    variableArray.push("niet_Deventer");
+  }
+
+  if(entry.includes("niet_Zwolle")){
+    variableArray.push("niet_Zwolle");
+  }
+
   if(entry.includes("platformNr") || entry.includes("spoorNr")){
     platformVar = document.getElementById('platformNrOptions_av' + av).value;
     platformVar = platformVar.replaceAll(/\'/g, "");
@@ -304,8 +320,8 @@ async function getSiGML(sentenceArray, av){
     }
     if (el == lastItem){
       // Extra pauze en eindpose toevoegen
-      tempStrings[av] += '<hamgestural_sign gloss=""><sign_nonmanual><head_tier><head_movement movement="SL" amount="1.5"/></head_tier><facialexpr_tier><eye_brows movement="RB" amount="0.7" speed="0.8"/><eye_lids movement="BB" speed="0.8"/><eye_gaze movement="LE" amount="0.6"/></facialexpr_tier></sign_nonmanual><sign_manual holdover="true"></sign_manual></hamgestural_sign>';
-      tempStrings[av] += '<hamgestural_sign gloss="" timescale=".8" duration="1.2"><sign_manual both_hands="true" lr_symm="true"><handconfig handshape="flat" thumbpos="across" /><split_handconfig><handconfig extfidir="ddl" palmor="l"/><handconfig extfidir="dr" palmor="r"/></split_handconfig><handconstellation contact="close"><location_hand location="palm" side="back"/><location_hand location="palm" side="palmar"/><location_bodyarm contact="close" location="belowstomach" second_location="belowstomach" side="right_at" second_side="front"/></handconstellation></sign_manual><sign_nonmanual><body_tier><body_movement movement="ST" /></body_tier><head_tier><head_par><head_movement movement="NU" amount="0.4"/><head_movement movement="SL" amount="1.5"/></head_par></head_tier><facialexpr_tier><facial_expr_par><eye_brows movement="RB" amount="0.6" /><eye_gaze movement="LE" amount="0.6"/><eye_lids movement="BB" /></facial_expr_par></facialexpr_tier></sign_nonmanual></hamgestural_sign>';
+      //tempStrings[av] += '<hamgestural_sign gloss=""><sign_nonmanual><head_tier><head_movement movement="SL" amount="1.5"/></head_tier><facialexpr_tier><eye_brows movement="RB" amount="0.7" speed="0.8"/><eye_lids movement="BB" speed="0.8"/><eye_gaze movement="LE" amount="0.6"/></facialexpr_tier></sign_nonmanual><sign_manual holdover="true"></sign_manual></hamgestural_sign>';
+      //tempStrings[av] += '<hamgestural_sign gloss="" timescale=".8" duration="1.2"><sign_manual both_hands="true" lr_symm="true"><handconfig handshape="flat" thumbpos="across" /><split_handconfig><handconfig extfidir="ddl" palmor="l"/><handconfig extfidir="dr" palmor="r"/></split_handconfig><handconstellation contact="close"><location_hand location="palm" side="back"/><location_hand location="palm" side="palmar"/><location_bodyarm contact="close" location="belowstomach" second_location="belowstomach" side="right_at" second_side="front"/></handconstellation></sign_manual><sign_nonmanual><body_tier><body_movement movement="ST" /></body_tier><head_tier><head_par><head_movement movement="NU" amount="0.4"/><head_movement movement="SL" amount="1.5"/></head_par></head_tier><facialexpr_tier><facial_expr_par><eye_brows movement="RB" amount="0.6" /><eye_gaze movement="LE" amount="0.6"/><eye_lids movement="BB" /></facial_expr_par></facialexpr_tier></sign_nonmanual></hamgestural_sign>';
       tempStrings[av] += '</sigml>';
     }
   }
