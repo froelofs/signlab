c73117882117cc6380871d4f2d64b1b1a7b3e20f
LykeEsselink <35260131+LykeEsselink@users.noreply.github.com>
2025-09-29 12:46:43 +0200

    Update website

--- js/general.js
+++ js/general.js
@@ -178,79 +178,97 @@ $('.timepicker').timepicker({
    return time;
   }
 
-  $(function () {
-    var bar = '';
-    var hoverTimeout;
+$(function () {
+  var bar = '';
+  var hoverTimeout;
 
-    bar += '<nav class="navbar navbar-shrink navbar-expand-lg navbar-light fixed-top" id="mainNav">';
-    bar += '<div class="container-fluid" style="width: 83%; margin-top: 0px;">';
-    bar += '<a class="navbar-brand" href="index.html">';
-    bar += '<img src="images/Xbreed_logo_tr.png" height="25">';
-    bar += '</a>';
-    bar += '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">';
-    bar += '<i class="fas fa-bars"></i>';
-    bar += ' Menu';
-    bar += '</button>';
-    bar += '<div class="collapse text-right navbar-collapse" style="margin-right:1%" id="navbarResponsive">';
-    bar += '<ul class="navbar-nav ml-auto" role="tablist">';
-    bar += '<li class="nav-item"><a class="nav-link" href="index.html" id="AboutNav">Welcome</a></li>';
-    // Research dropdown
-    bar += '<li class="nav-item dropdown">';
-    bar += '<a class="nav-link dropdown-toggle" href="#" id="ResearchNav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Research</a>';
-    bar += '<div class="dropdown-menu" aria-labelledby="ResearchNav">';
-    bar += '<a class="dropdown-item" href="research-overview.html" id="OverviewNav">Overview</a>';
-    bar += '<a class="dropdown-item" href="research-methods.html" id="MethodsNav">New Methods</a>';
-    bar += '<a class="dropdown-item" href="research-datasets.html" id="DatasetsNav">Datasets</a>';
-    bar += '<a class="dropdown-item" href="research-theory.html" id="TheoryNav">Theory</a>';
-    bar += '<a class="dropdown-item" href="research-applications.html" id="ApplicationsNav">Applications</a>';
-    bar += '</div>';
-    bar += '</li>';
-    bar += '<li class="nav-item"><a class="nav-link" href="people.html" id="PeopleNav">People</a></li>';
-    bar += '<li class="nav-item"><a class="nav-link" href="publications.html" id="PublicationsNav">Publications</a></li>';
-    bar += '<li class="nav-item"><a class="nav-link" href="projects.html" id="ProjectsNav">Projects</a></li>';
-    bar += '<li class="nav-item"><a class="nav-link" href="media.html" id="MediaNav">Media</a></li>';
-    bar += '<li class="nav-item"><a class="nav-link" href="signopsis.html" id="SignopsisNav">Signopsis</a></li>';
-    bar += '</ul>';
-    bar += '</div>';
-    bar += '</div>';
-    bar += '</nav>';
+  bar += '<nav class="navbar navbar-shrink navbar-expand-lg navbar-light fixed-top" id="mainNav">';
+  bar += '<div class="container-fluid" style="width: 83%; margin-top: 0px;">';
+  bar += '<a class="navbar-brand" href="index.html">';
+  bar += '<img src="images/Xbreed_logo_tr.png" height="25">';
+  bar += '</a>';
+  bar += '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">';
+  bar += '<i class="fas fa-bars"></i>';
+  bar += ' Menu';
+  bar += '</button>';
+  bar += '<div class="collapse text-right navbar-collapse" style="margin-right:1%" id="navbarResponsive">';
+  bar += '<ul class="navbar-nav ml-auto" role="tablist">';
+  bar += '<li class="nav-item"><a class="nav-link" href="index.html" id="AboutNav">Welcome</a></li>';
+  // Research dropdown
+  bar += '<li class="nav-item dropdown">';
+  bar += '<a class="nav-link dropdown-toggle" href="#" id="ResearchNav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Research</a>';
+  bar += '<div class="dropdown-menu" aria-labelledby="ResearchNav">';
+  bar += '<a class="dropdown-item" href="research-overview.html" id="OverviewNav">Overview</a>';
+  bar += '<a class="dropdown-item" href="research-methods.html" id="MethodsNav">New Methods</a>';
+  bar += '<a class="dropdown-item" href="research-datasets.html" id="DatasetsNav">Datasets</a>';
+  bar += '<a class="dropdown-item" href="research-theory.html" id="TheoryNav">Theory</a>';
+  bar += '<a class="dropdown-item" href="research-applications.html" id="ApplicationsNav">Applications</a>';
+  bar += '<a class="dropdown-item" href="publications.html" id="PublicationsNav">Publications</a>';
+  bar += '<a class="dropdown-item" href="projects.html" id="ProjectsNav">Projects</a>';
+  bar += '</div>';
+  bar += '</li>';
+  // bar += '<li class="nav-item"><a class="nav-link" href="news.html" id="NewsNav">News</a></li>';
+  // Outreach dropdown
+  bar += '<li class="nav-item dropdown">';
+  bar += '<a class="nav-link dropdown-toggle" href="#" id="OutreachNav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Outreach</a>';
+  bar += '<div class="dropdown-menu" aria-labelledby="OutreachNav">';
+  bar += '<a class="dropdown-item" href="outreach-events.html" id="EventsNav">Outreach Events</a>';
+  bar += '<a class="dropdown-item" href="media.html" id="MediaNav">Media</a>';
+  bar += '<a class="dropdown-item" href="signopsis.html" id="SignopsisNav">Signopsis</a>';
+  bar += '</div>';
+  bar += '</li>';
+  bar += '<li class="nav-item"><a class="nav-link" href="people.html" id="PeopleNav">People</a></li>';
+    // Nederlands dropdown
+  bar += '<li class="nav-item dropdown">';
+  bar += '<a class="nav-link dropdown-toggle" href="#" id="NederlandsNav" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Nederlands</a>';
+  bar += '<div class="dropdown-menu" aria-labelledby="NederlandsNav">';
+  bar += '<a class="dropdown-item" href="evenementen.html" id="EvenementenNav">Evenementen</a>';
+  bar += '<a class="dropdown-item" href="onderzoek.html" id="OnderzoekNav">Onderzoek</a>';
+  bar += '</div>';
+  bar += '</li>';
+  bar += '</ul>';
+  bar += '</div>';
+  bar += '</div>';
+  bar += '</nav>';
 
-    $("#nav-placeholder").html(bar);
+  $("#nav-placeholder").html(bar);
 
-    // Add hover functionality for dropdowns
-    $(".nav-item.dropdown").hover(
-      function () {
-          clearTimeout(hoverTimeout);
-          $(this).addClass("show");
-          $(this).find(".dropdown-menu").addClass("show");
-      },
-      function () {
-          var that = $(this);
-          hoverTimeout = setTimeout(function () {
-              that.removeClass("show");
-              that.find(".dropdown-menu").removeClass("show");
-          }, 200);  // 200ms delay before hiding
-      }
-  );
+  // Add hover functionality for dropdowns
+$(".nav-item.dropdown").hover(
+  function () {
+    clearTimeout(hoverTimeout);
 
-    // Highlight active page
-    var id = getPage();
-    console.log("navbar id", id);
-    $("#" + id).addClass("active");
+    // Close other open dropdowns
+    $(".nav-item.dropdown").not(this).removeClass("show").find(".dropdown-menu").removeClass("show");
+
+    // Open current dropdown
+    $(this).addClass("show");
+    $(this).find(".dropdown-menu").addClass("show");
+  },
+  function () {
+    var that = $(this);
+    hoverTimeout = setTimeout(function () {
+        that.removeClass("show");
+        that.find(".dropdown-menu").removeClass("show");
+    }, 200);  // 200ms delay before hiding
+  }
+);
+
+  // Highlight active page
+  var id = getPage();
+  console.log("navbar id", id);
+  $("#" + id).addClass("active");
 });
 
 // Function to determine the active page
 function getPage() {
-    const allTabs = ['AboutNav', 'ResearchNav', 'PeopleNav', 'PublicationsNav', 'ProjectsNav', 'MediaNav', 'SignopsisNav', 'PlaygroundNav'];
+    const allTabs = ['AboutNav', 'ResearchNav', 'PeopleNav', 'OutreachNav', 'NederlandsNav'];
     const sections = [
         'About',
-        ['Overview', 'Methods', 'Datasets', 'Theory', 'Applications'], // Research dropdown sections
+        ['Overview', 'Methods', 'Datasets', 'Theory', 'Applications', 'Publications', 'Projects'], // Research dropdown sections
         'People',
-        'Publications',
-        'Projects',
-        'Media',
-        'Signopsis',
-        'Playground'
+        ['Events', 'Media', 'Signopsis'],
+        ['Onderzoek', 'Evenementen']
     ];
 
     for (let j = 0; j < sections.length; j++) {
@@ -268,3 +286,24 @@ function getPage() {
         }
     }
 }
+
+// Function to get the NavBar height for the sticky text
+function adjustStickyOffset() {
+  var navbarHeight = $('#mainNav').outerHeight();
+  
+  // Set CSS variable on <body> or root element
+  document.documentElement.style.setProperty('--navbar-height', navbarHeight + 'px');
+  
+  // Alternatively, update top style on sticky elements directly
+  $('.sticky-top').css('top', navbarHeight + 'px');
+}
+
+// Run on page load
+$(document).ready(function(){
+  adjustStickyOffset();
+});
+
+// Run on window resize to adapt to height changes
+$(window).resize(function(){
+  adjustStickyOffset();
+});
